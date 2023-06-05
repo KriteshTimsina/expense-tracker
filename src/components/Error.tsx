ffcf9f785c37376f8c177416ff9dbf726898ab11
Kritesh Timsina <kriteshtimsina55@gmail.com>
2023-06-05 17:39:29 +0545

    fix:adjust error component position in small devices

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -5,7 +5,7 @@ const Error = () => {
   return (
     <div>
       {error && (
-        <div className="absolute right-0 flex items-center justify-center p-5 text-lg text-white bg-orange-500 bottom-10 animate-slide">
+        <div className="absolute right-0 flex items-center justify-center p-3 text-lg text-white bg-orange-500 top-2/3 md:top-1/2 animate-slide">
           Please Fill all the fields.
         </div>
       )}
